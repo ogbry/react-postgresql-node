67aefa82e3e397a1160ea81b26b07200fb3e5221
clarkamor <clark.amor@boom.camp>
2020-03-17 11:04:00 +0800

    none

--- client/src/components/login/Template.js
+++ client/src/components/login/Template.js
@@ -1,27 +1,12 @@
 import React from "react";
-import Box from "@material-ui/core/Box";
-import { makeStyles } from "@material-ui/core/styles";
 import Login from "./Login";
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    justifyContent: "center",
-    display: "flex"
-  },
-  paper: {
-    margin: 400
-  }
-}));
-
-export default function SignInSide() {
-  const classes = useStyles();
-
+export default function Template() {
   return (
-    <div className={classes.root}>
-      <div className={classes.paper}>
-        <form className={classes.form} noValidate>
+    <div className="root">
+      <div className="paper">
+        <form className="form" noValidate>
           <Login />
-          <Box mt={5}></Box>
         </form>
       </div>
     </div>
--- client/src/routes/private_routes.js
+++ client/src/routes/private_routes.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
-
+import cookies from "js-cookie";
 export default function PrivateRoutes({ component: Component }) {
-  const token = localStorage.getItem("token");
+  const token = cookies.get("token");
   return (
     <Route
       render={props => (token ? <Component {...props} /> : <Redirect to="/" />)}
